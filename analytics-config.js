f17e4ff8225861eb022dcbb078bcf0317104796f
Patrick Dudley IV <patduds@Patricks-Air.attlocal.net>
2026-05-28 21:51:35 -0700

    Enable GA4 analytics

--- analytics-config.js
+++ analytics-config.js
@@ -1,4 +1,4 @@
 window.DIVEPRO_ANALYTICS = {
   // Create a GA4 web data stream and paste the Measurement ID here, for example: "G-ABC123XYZ0".
-  ga4MeasurementId: "",
+  ga4MeasurementId: "G-0N8YNBB36M",
 };
